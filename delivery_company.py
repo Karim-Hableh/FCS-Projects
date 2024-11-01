1009d1b1d177f5dc08c4433a22cdf1dbeade8140
unknown <hableh_hatab@icloud.com>
2024-11-01 22:22:43 +0200

    view_drivers

--- delivery_company.py
+++ delivery_company.py
@@ -19,8 +19,42 @@ class WeDeliver:
                 self.cities_menu()
             elif choice==3:
                 print("good bye!")
+                break
 
             else:
                 print("invalid choice please try again")
-        
-        
+
+    def drivers_menu(self):
+        while True:
+            print("Enter")
+            print("1. To view all the drivers")
+            print("2. To add a driver")
+            print("3. Check similar drivers")
+            print("4. To go back to the main menu")
+
+            choice=input("Enter choice:")
+
+            if choice==1:
+                self.viewDrivers()
+            elif choice==2:
+                self.addDrivers()
+            elif choice==3:
+                self.checkDrivers()
+            elif choice==4:
+                break
+            else:
+                print("invalid choice please try again")
+
+    def generate_driver_id(self):
+        driver_id=f"ID{self.driver_id_counter:03}"
+        self.driver_id_counter+=1
+        return driver_id
+    
+
+    def viewDrivers(self):
+        print("The list of drivers is:")
+        if self.drivers==0:
+            print("No drivers in the system")
+        else:
+            for driver_id,(name,startCity) in self.drivers.items():
+                print(f"{driver_id},{name},{startCity}")
